2d4804716be747c81c949c3276083ae10dece51c
yarivtenta-creator <yarivtenta-creator@gmail.com>
2026-03-24 05:46:05 +0100

    Add test page

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{padding: '40px', color: '#fff'}}>
+      <h1>✓ App is WORKING!</h1>
+      <p>If you see this, the deployment is successful.</p>
+      <p>Go to <a href="/" style={{color: '#3b82f6'}}>/</a> to go home.</p>
+    </div>
+  );
+}
